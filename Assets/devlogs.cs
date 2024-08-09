ca0323fe15ae9b84049740a2a1de6c1ff23bb86b
Y-L77 <streamluu@gmail.com>
2024-08-10 05:42:37 +1000

    water physics
    changed up the tilemaps, fixed update for the swimming physics and added water velocity multiplier on enter / exit

--- Assets/devlogs.cs
+++ Assets/devlogs.cs
@@ -7,9 +7,14 @@ public class devlogs : MonoBehaviour
     //reminder to always use lowercase in tags, unity is case sensitive please don't fuck up and spend hours debugging because of a mispelt tag.
 
 
-    //2024/08/09
+    //2024/08/08
     /*
      Yipeng - I will make player movement script and buoyancy calculator with rigidbody, if it doesn't work ill make the physics myself.
     I am also planning on making camera follow player after it touches a hidden trigger just above the water
+
+
+
+    2024/08/09
+    The lobby camera zoom will be 9, once the player dives into the water it will change to ~7 and follow the player.
     */
 }
--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -10,6 +11,7 @@ public class playerMovement : MonoBehaviour
     private float jumpHeight = 5.5f;
     public bool submerged = false;
     public float speed = 4f;
+    public float swimForce = 10f;
 
     private void Update() //add coyote jump to this later
     {
@@ -33,11 +35,33 @@ public class playerMovement : MonoBehaviour
             // Flip the player sprite based on movement direction
             if (moveInput < 0) // Move left
             {
-                player.transform.localScale = new Vector3(-0.5f, 0.5f, 1); // Flip left
+                player.transform.rotation = Quaternion.Euler(0, 180, 0);
             }
             else if (moveInput > 0) // Move right
             {
-                player.transform.localScale = new Vector3(0.5f, 0.5f, 1); // Flip right
+                player.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+    }
+
+    private void FixedUpdate() //swimming is physics heavy so this method will be used to get swimming inputs
+    {
+        if (submerged)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+            // Adjust the multiplier based on how strong you want the swimming force to be
+            Rigidbody2D.AddForce(movement * swimForce);
+
+            float rotationSpeed = 2f;
+            float angle = Mathf.Atan2(Rigidbody2D.velocity.y, Rigidbody2D.velocity.x) * Mathf.Rad2Deg;
+
+            if (Rigidbody2D.velocity.magnitude > 0.1f)
+            { // Only rotate when moving
+                Rigidbody2D.rotation = Mathf.LerpAngle(Rigidbody2D.rotation, angle, rotationSpeed * Time.fixedDeltaTime);
             }
         }
     }
@@ -53,6 +77,28 @@ public class playerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("ground")) // Check if the collided object is tagged as "ground"
         {
             isGrounded = false; // Set grounded status to false
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("water"))
+        {
+            submerged = true;
+            player.transform.rotation = Quaternion.Euler(0, 0, -135);
+            Rigidbody2D.drag = 5f;
+            Rigidbody2D.angularDrag = 10f;
+            Rigidbody2D.gravityScale = 0.1f;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("water"))
+        {
+            submerged = false;
+            Rigidbody2D.drag = 0f;
+            Rigidbody2D.angularDrag = 0.05f;
+            Rigidbody2D.gravityScale = 1f;
+            Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x, Rigidbody2D.velocity.y * 3f);
         }
     }
 }
